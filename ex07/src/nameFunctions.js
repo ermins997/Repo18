c987e43060dfbeb6adf6f2115d49ea41dea3e661
Ermin Sacic <sacic_ermin97@hotmail.com>
2022-04-15 09:36:15 +0200

    da

--- ex07/src/nameFunctions.js
+++
@@ -1,9 +0,0 @@
-let firstName = (firstName) => {
-    return firstName.toUpperCase();
-  };
-  let lastName = (lastName) => {
-    return lastName.toLowerCase();
-  };
-  
-  exports.firstName = firstName;
-  exports.lastName = lastName;
